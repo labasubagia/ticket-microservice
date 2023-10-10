aeac428259bf175b1d167c65fc5a3a4168ebaed8
Laba Subagia <labasubagia22@gmail.com>
2023-10-10 12:16:39 +0800

    feat: current user

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,10 @@ import { json } from "express";
 const app = express()
 app.use(json())
 
+app.get('/api/users/current-user', (req, res) => {
+    res.send('Hi there!')
+})
+
 app.listen(3000, () => {
     console.log('Listening to port 3000!')
 })
