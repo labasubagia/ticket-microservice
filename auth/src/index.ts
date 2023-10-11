f6db8472cb93a062efdf0ad25a9e4069088b3ad6
Laba Subagia <labasubagia22@gmail.com>
2023-10-11 21:19:58 +0800

    feat: routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,18 @@
 import express from "express";
 import { json } from "express";
+import { currentUserRouter } from "./routes/current-user";
+import { signInRouter } from "./routes/sign-in";
+import { signUpRouter } from "./routes/sign-up";
+import { signOutRouter } from "./routes/sign-out";
 
 const app = express()
 app.use(json())
 
-app.get('/api/users/current-user', (req, res) => {
-    res.send('Hi there!')
-})
+app.use(currentUserRouter)
+app.use(signUpRouter)
+app.use(signInRouter)
+app.use(signOutRouter)
 
 app.listen(3000, () => {
-    console.log('Listening to port 3000!')
+    console.log('listening to port 3000!')
 })
