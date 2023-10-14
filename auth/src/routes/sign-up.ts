b0b9d432ef06074cb04377718b481409dd40e93c
Laba Subagia <labasubagia22@gmail.com>
2023-10-14 18:46:06 +0800

    feat: add user sign up

--- auth/src/routes/sign-up.ts
+++ auth/src/routes/sign-up.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from 'express'
 import { body, validationResult } from 'express-validator'
+import { User } from '../models/user'
 import { RequestValidationError } from '../errors/request-validation-error'
-import { DatabaseConnectionError } from '../errors/database-connection-error'
+import { BadRequestError } from '../errors/bad-request-error'
 
 const router = express.Router()
 
@@ -21,10 +22,17 @@ router.post('/api/users/sign-up',
         if (!errors.isEmpty()) {
             throw new RequestValidationError(errors.array())
         }
-        const { email, password } = req.body
-        throw new DatabaseConnectionError()
 
-        res.send('Hi there!')
+        const { email, password } = req.body
+        const existingUser = await User.findOne({ email })
+        if (existingUser) {
+            throw new BadRequestError('Email in use')
+        }
+
+        const user = User.build({ email, password })
+        await user.save()
+
+        return res.status(201).send(user)
     },
 )
 
