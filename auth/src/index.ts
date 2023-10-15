35d078e74fb2fd8e875c8a2f0e93f703250ecc1b
Laba Subagia <labasubagia22@gmail.com>
2023-10-16 00:31:20 +0800

    feat: use cookie session

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import 'express-async-errors'
 import { json } from "express";
+import cookieSession from "cookie-session";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signInRouter } from "./routes/sign-in";
@@ -11,7 +12,14 @@ import { NotFoundError } from "./errors/not-found-error";
 import mongoose from "mongoose";
 
 const app = express()
+app.set('trust proxy', true)
 app.use(json())
+app.use(
+    cookieSession({
+        signed: false,
+        secure: true,
+    })
+)
 
 app.use(currentUserRouter)
 app.use(signUpRouter)
