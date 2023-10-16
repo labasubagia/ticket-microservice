033a288f4202f5b2ac4d4001a2d12330e4e994d1
Laba Subagia <labasubagia22@gmail.com>
2023-10-17 03:54:02 +0800

    test: sign in route

---
+++ auth/src/routes/__test__/sign-in.test.ts
@@ -0,0 +1,52 @@
+import request from 'supertest'
+import { app } from '../../app'
+
+it('fails when email does not exists', async () => {
+    return request(app)
+        .post('/api/users/sign-in')
+        .send({
+            email: "test@test.com",
+            password: "12345"
+        })
+        .expect(400)
+})
+
+
+it('fails when password incorrect', async () => {
+    await request(app)
+        .post('/api/users/sign-up')
+        .send({
+            email: "test@test.com",
+            password: "12345"
+        })
+        .expect(201)
+
+    await request(app)
+        .post('/api/users/sign-in')
+        .send({
+            email: "test@test.com",
+            password: "invalid_password"
+        })
+        .expect(400)
+})
+
+
+it('set a cookie after successful sign up', async () => {
+    await request(app)
+        .post('/api/users/sign-up')
+        .send({
+            email: "test@test.com",
+            password: "12345"
+        })
+        .expect(201)
+
+    const response = await request(app)
+        .post('/api/users/sign-in')
+        .send({
+            email: "test@test.com",
+            password: "12345"
+        })
+        .expect(200)
+
+    expect(response.get('Set-Cookie')).toBeDefined()
+})
