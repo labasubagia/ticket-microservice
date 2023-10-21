82e515cf61affce2e56d62750e9b7263d6d52dc0
Laba Subagia <labasubagia22@gmail.com>
2023-10-22 01:59:50 +0800

    feat: add login page

---
+++ client/app/login/page.tsx
@@ -0,0 +1,68 @@
+'use client'
+
+import * as z from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
+
+const formSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(4).max(20)
+})
+
+export default function Login() {
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    }
+  })
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    // TODO: Submit here
+    console.log(values)
+  }
+
+  return (
+    <main className={cn('p-4 px-8')}>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
+          <FormField 
+            control={form.control}
+            name='email'
+            render={({field}) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder='Email' {...field}/>
+                </FormControl>
+                <FormMessage/>
+              </FormItem>
+            )}
+          />
+
+          <FormField 
+            control={form.control}
+            name='password'
+            render={({field}) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input placeholder='Password' type='password' {...field}/>
+                </FormControl>
+                <FormMessage/>
+              </FormItem>
+            )}
+          />
+
+          <Button type="submit">Login</Button>
+        </form>
+      </Form>
+    </main>
+  )
+}
