4feaf6eeb9551773183c7912f5f993e1ff1201d6
Laba Subagia <labasubagia22@gmail.com>
2023-10-26 23:22:18 +0800

    feat: react query provider

---
+++ client/components/providers/react-query.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { useState } from "react"
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+
+const ReactQueryProvider = ({ children} : { children: React.ReactNode }) => {
+  const  [queryClient] = useState(() => new QueryClient())
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false}/>
+    </QueryClientProvider>
+  )
+}
+
+export default ReactQueryProvider
