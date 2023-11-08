e73f95721c84a90054c98c1f4670ed1994df4a11
Laba Subagia <labasubagia22@gmail.com>
2023-11-09 05:31:27 +0800

    feat: use router

--- order/src/app.ts
+++ order/src/app.ts
@@ -4,6 +4,11 @@ import { currentUser, errorHandler, NotFoundError } from '@klstickets/common'
 import cookieSession from 'cookie-session'
 import express, { json } from 'express'
 
+import { createOrderRouter } from '@/routes/create'
+import { deleteOrderRouter } from '@/routes/delete'
+import { listOrderRouter } from '@/routes/list'
+import { showOrderRouter } from '@/routes/show'
+
 const app = express()
 app.set('trust proxy', true)
 app.use(json())
@@ -13,7 +18,13 @@ app.use(
     secure: process.env.NODE_ENV !== 'test'
   })
 )
+
 app.use(currentUser)
+
+app.use(listOrderRouter)
+app.use(showOrderRouter)
+app.use(createOrderRouter)
+app.use(deleteOrderRouter)
 
 app.use(async (req, res, next) => {
   throw new NotFoundError()
