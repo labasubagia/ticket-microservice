76507e78dc3bb44442edd8e571d8ec5ddb195726
Laba Subagia <labasubagia22@gmail.com>
2023-11-10 20:57:13 +0800

    chore: fix consumer when stream not found

--- tmp/nats-try/src/jetstream/base-consumer.ts
+++ tmp/nats-try/src/jetstream/base-consumer.ts
@@ -1,4 +1,11 @@
-import { AckPolicy, DeliverPolicy, JsMsg, NatsConnection } from "nats";
+import {
+  AckPolicy,
+  DeliverPolicy,
+  ErrorCode,
+  JsMsg,
+  NatsConnection,
+  NatsError,
+} from "nats";
 import { Event } from "./types";
 
 export abstract class Consumer<T extends Event> {
@@ -13,12 +20,38 @@ export abstract class Consumer<T extends Event> {
 
   async init() {
     const jsm = await this.client.jetstreamManager();
+
+    const stream = await jsm.streams.get(this.topic).catch(async (error) => {
+      const isNotFound =
+        (error as NatsError)?.code === ErrorCode.JetStream404NoMessages;
+      if (isNotFound) {
+        // add stream
+        await jsm.streams.add({
+          name: this.topic,
+          subjects: [this.subject],
+        });
+      } else {
+        console.error(error);
+      }
+    });
+
+    if (stream) {
+      // update stream
+      const streamInfo = await stream.info();
+      if (!streamInfo.config.subjects.includes(this.subject)) {
+        streamInfo.config.subjects?.push(this.subject);
+        await jsm.streams.update(this.topic, streamInfo.config);
+      }
+    }
+
+    // add consumer
     await jsm.consumers.add(this.topic, {
       durable_name: this.queueGroupName,
       deliver_policy: DeliverPolicy.All,
       ack_policy: AckPolicy.Explicit,
       filter_subject: this.subject,
     });
+
     return this;
   }
 
