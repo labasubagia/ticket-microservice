da5d110817d2786709bd65e0227513d4048bd82c
Laba Subagia <labasubagia22@gmail.com>
2023-11-12 00:43:42 +0800

    feat: concurrency control listener

--- order/src/events/consumers/ticket-updated-consumer.ts
+++ order/src/events/consumers/ticket-updated-consumer.ts
@@ -1,5 +1,9 @@
-import type { TicketUpdatedEvent } from '@klstickets/common'
-import { Consumer, Subject, Topic } from '@klstickets/common'
+import {
+  Consumer,
+  Subject,
+  type TicketUpdatedEvent,
+  Topic
+} from '@klstickets/common'
 
 import { queueGroupName } from '@/events/consumers/queue-group-name'
 import { Ticket } from '@/models/ticket'
@@ -12,11 +16,11 @@ class TicketUpdatedConsumer extends Consumer<TicketUpdatedEvent> {
   failRetryWaitMs: number = 1000
 
   async onMessage(data: TicketUpdatedEvent['data']): Promise<void> {
-    const { id, title, price } = data
-    const ticket = await Ticket.findById(id)
+    const ticket = await Ticket.findByEvent(data)
     if (ticket == null) {
       throw new Error('Ticket not found')
     }
+    const { title, price } = data
     ticket.set({ title, price })
     await ticket.save()
   }
