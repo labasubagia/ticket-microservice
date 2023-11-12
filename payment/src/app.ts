f99cb114c8da41b53ebff908f1f1a961370ffe90
Laba Subagia <labasubagia22@gmail.com>
2023-11-13 07:57:05 +0800

    feat: add payment svc

---
+++ payment/src/app.ts
@@ -0,0 +1,23 @@
+import 'express-async-errors'
+
+import { currentUser, errorHandler, NotFoundError } from '@klstickets/common'
+import cookieSession from 'cookie-session'
+import express, { json } from 'express'
+
+const app = express()
+app.set('trust proxy', true)
+app.use(json())
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test'
+  })
+)
+app.use(currentUser)
+
+app.use(async (req, res, next) => {
+  throw new NotFoundError()
+})
+app.use(errorHandler)
+
+export { app }
---
+++ payment/src/index.ts
@@ -0,0 +1,57 @@
+import mongoose from 'mongoose'
+
+import { app } from '@/app'
+import { natsWrapper } from '@/nats-wrapper'
+
+const start = async (): Promise<void> => {
+  const jwtKey = process.env.JWT_KEY ?? ''
+  if (jwtKey === '') {
+    throw new Error('JWT_KEY must be defined')
+  }
+
+  const mongoUri = process.env.MONGO_URI ?? ''
+  if (mongoUri === '') {
+    throw new Error('MONGO_URI must be defined')
+  }
+
+  const natsServers = (process.env.NATS_SERVERS ?? '').split(',')
+  if (natsServers.length === 0) {
+    throw new Error('NATS_SERVERS must be defined')
+  }
+
+  try {
+    // mongo
+    await mongoose.connect(mongoUri)
+    console.log('connected to mongodb')
+
+    // nats
+    await natsWrapper.connect(natsServers)
+
+    natsWrapper.client.closed().catch((error) => {
+      console.log('NATS closed', error.message)
+      process.exit()
+    })
+
+    // server
+    const server = app.listen(3000, () => {
+      console.log('listening to port 3000!')
+    })
+
+    const onClose = async (signal: NodeJS.Signals): Promise<void> => {
+      await natsWrapper.client.close()
+      await mongoose.connection.close()
+      server.close(() => {
+        console.log('Http server closed')
+        process.exit()
+      })
+    }
+
+    process.on('SIGINT', onClose)
+    process.on('SIGTERM', onClose)
+  } catch (error) {
+    console.error(error)
+    process.exit()
+  }
+}
+
+void start()
