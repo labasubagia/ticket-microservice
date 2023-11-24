1c21f208a2410067f96c5d49fec562907b1935fa
Laba Subagia <labasubagia22@gmail.com>
2023-11-24 21:44:24 +0800

    feat: list ticket

--- client/app/(root)/page.tsx
+++ client/app/(root)/page.tsx
@@ -1,7 +1,57 @@
-export default async function Home() {
+'use client';
+
+import { getCurrentUser } from '@/actions/auth';
+import { list } from '@/actions/ticket';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { useQuery } from '@tanstack/react-query';
+import Link from 'next/link';
+
+export default function ListTicketPage() {
+  const currentUserQuery = useQuery({
+    queryKey: ['currentUser'],
+    queryFn: getCurrentUser,
+    networkMode: 'offlineFirst',
+  });
+
+  const listTicketQuery = useQuery({
+    queryKey: ['listTicket'],
+    queryFn: list,
+  });
+
   return (
-    <>
-      <p>Hello World</p>
-    </>
+    <div className="p-4 px-8">
+      <div className="flex items-center justify-between pb-4">
+        <h1 className="text-xl">Tickets</h1>
+        {currentUserQuery.data && (
+          <Button asChild>
+            <Link href={'/ticket/create'}>Sell My Ticket</Link>
+          </Button>
+        )}
+      </div>
+
+      {listTicketQuery.data?.map((ticket) => (
+        <Card key={ticket.id} className="mb-4">
+          <CardHeader>
+            <CardTitle>{ticket.title}</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="flex items-center justify-between">
+              <p>${ticket.price}</p>
+              <div className="flex justify-between space-x-2">
+                <Button variant={'secondary'}>
+                  <Link href={`/ticket/${ticket.id}`}>Detail</Link>
+                </Button>
+                {ticket.userId == currentUserQuery.data?.id && (
+                  <Button variant={'destructive'}>
+                    <Link href={`/ticket/edit/${ticket.id}`}>Edit</Link>
+                  </Button>
+                )}
+              </div>
+            </div>
+          </CardContent>
+        </Card>
+      ))}
+    </div>
   );
 }
