9810b987907ae1c6fc50bb5d884fa9f9db6e576a
Laba Subagia <labasubagia22@gmail.com>
2023-11-25 18:27:54 +0800

    chore: update types

--- client/types/order.ts
+++ client/types/order.ts
@@ -2,9 +2,28 @@ import { Ticket } from '@/types/ticket';
 
 export interface Order {
   userId: string;
-  status: string;
+  status: OrderStatus;
   expiresAt: string;
   ticket: Ticket;
   version: number;
   id: string;
 }
+
+export enum OrderStatus {
+  Created = 'created',
+  AwaitingPayment = 'awaiting:payment',
+  Cancelled = 'cancelled',
+  Complete = 'complete',
+}
+
+export const getOrderExpiresDiff = (order: Order) => {
+  return new Date(order.expiresAt).getTime() - new Date().getTime();
+};
+
+export const isOrderFinished = (order: Order) => {
+  return [OrderStatus.Cancelled, OrderStatus.Complete].includes(order.status);
+};
+
+export const isOrderPending = (order: Order) => {
+  return getOrderExpiresDiff(order) > 0 && !isOrderFinished(order);
+};
--- client/types/ticket.ts
+++ client/types/ticket.ts
@@ -1,9 +1,11 @@
 import { User } from '@/types/user';
+import { Order } from './order';
 
 export interface Ticket {
   id: string;
   version: number;
   userId: User['id'];
+  orderId?: Order['id'];
   title: string;
   price: number;
 }
